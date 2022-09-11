4124e7941890b9ac8ca629c1dc9bcbcf2b9a5eee
Quinn Shultz <vanoxite@gmail.com>
2022-09-11 13:59:32 -0400

    Added a headless PowerShell script create a Windows package of the game.

---
+++ build.ps1
@@ -0,0 +1,26 @@
+# Before running this script, set the UNREAL_HOME environment variable 
+# (i.e. "SET UNREAL_HOME <path_to_unreal>")
+
+$UNREAL_HOME="D:\Program Files\Epic Games\UE_4.26Chaos"
+$PROJECT_HOME=(Get-Location).Path
+
+if($UNREAL_HOME -eq $null)
+{
+	Write-Output "Please set the UNREAL_HOME environment variable!"
+}
+
+Write-Output "REMOVING OLD BRONCODROME ARTIFACTS"
+Remove-Item -Recurse Output\*
+Write-Output "ARTIFACTS REMOVED"
+
+Write-Output "COMPILING"
+Set-Location $UNREAL_HOME\Engine\Build\BatchFiles
+Start-Process -NoNewWindow -FilePath "$UNREAL_HOME\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
+
+if($COOK_PROJECT -eq $True)
+{
+    Write-Output "COOKING"
+    Start-Process -NoNewWindow -FilePath "$UNREAL_HOME\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -NoCompile -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
+}
+
+Set-Location $PROJECT_HOME
