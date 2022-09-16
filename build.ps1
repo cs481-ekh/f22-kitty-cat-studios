9a50023a7c08682bdf75284ba5f03607889ceaef
Quinn Shultz <quinnshultz@u.boisestate.edu>
2022-09-16 11:39:17 -0600

    Closes #21 (#29)

---
+++ build.ps1
@@ -0,0 +1,25 @@
+# Before running this script, set the UNREAL_HOME environment variable
+# (i.e. "SET UNREAL_HOME <path_to_unreal>")
+
+$PROJECT_HOME=(Get-Location).Path
+
+if($null -eq $UNREAL_HOME)
+{
+	Write-Output "Please set the UNREAL_HOME environment variable!"
+}
+
+Write-Output "REMOVING OLD BRONCODROME ARTIFACTS"
+Remove-Item -Recurse Output\*
+Write-Output "ARTIFACTS REMOVED"
+
+Write-Output "COMPILING"
+Set-Location $UNREAL_HOME\Engine\Build\BatchFiles
+Start-Process -NoNewWindow -FilePath "$UNREAL_HOME\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
+
+if($COOK_PROJECT -eq $True)
+{
+    Write-Output "COOKING"
+    Start-Process -NoNewWindow -FilePath "$UNREAL_HOME\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -NoCompile -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
+}
+
+Set-Location $PROJECT_HOME
