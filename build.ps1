da86650994795265aa7a1c67c129d4332eaa5674
Quinn Shultz <vanoxite@gmail.com>
2022-09-27 23:56:38 -0400

    Hardcoded some values to ensure build success.

--- build.ps1
+++ build.ps1
@@ -2,24 +2,20 @@
 # (i.e. "SET UNREAL_HOME <path_to_unreal>")
 
 $PROJECT_HOME=(Get-Location).Path
-
-if($null -eq $UNREAL_HOME)
-{
-	Write-Output "Please set the UNREAL_HOME environment variable!"
-}
+$COOK_PROJECT=True
 
 Write-Output "REMOVING OLD BRONCODROME ARTIFACTS"
 Remove-Item -Recurse Output\*
 Write-Output "ARTIFACTS REMOVED"
 
 Write-Output "COMPILING"
-Set-Location $UNREAL_HOME\Engine\Build\BatchFiles
-Start-Process -NoNewWindow -FilePath "$UNREAL_HOME\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
+Set-Location "D:\Program Files\Epic Games\UE_4.26Chaos\Engine\Build\BatchFiles"
+Start-Process -NoNewWindow -FilePath "D:\Program Files\Epic Games\UE_4.26Chaos\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
 
 if($COOK_PROJECT -eq $True)
 {
     Write-Output "COOKING"
-    Start-Process -NoNewWindow -FilePath "$UNREAL_HOME\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -NoCompile -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
+    Start-Process -NoNewWindow -FilePath "D:\Program Files\Epic Games\UE_4.26Chaos\Engine\Build\BatchFiles\RunUAT" -ArgumentList "BuildCookRun -project=$PROJECT_HOME\BroncoDrome.uproject -noP4 -platform=Win64 -clientconfig=Development -serverconfig=Development -cook -allmaps -NoCompile -stage -pak -archive -archivedirectory=$PROJECT_HOME\Output"
 }
 
 Set-Location $PROJECT_HOME
