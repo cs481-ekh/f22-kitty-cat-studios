2f22a58641496f58b0b47cb67acfeb86ff6bcef7
Cameron Sewell <77357348+CameronSewell@users.noreply.github.com>
2022-10-04 11:20:40 -0600

    created .cpp and .h files

---
+++ Source/BroncoDrome/Runner/Stats.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChaosVehicleWheel.h"
+#include "RunnerWheel.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class BRONCODROME_API UStats : public UChaosVehicleWheel
+{
+	GENERATED_BODY()
+
+public:		// Constructors
+
+	URunnerWheel();
+
+};
