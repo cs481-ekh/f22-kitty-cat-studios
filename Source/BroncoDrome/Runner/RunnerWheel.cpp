471285c658f88f803c978955f9396d88d18f7ddd
Cameron Sewell <77357348+CameronSewell@users.noreply.github.com>
2022-10-11 13:44:09 -0600

    Runner stats class (#193)
    * created .cpp and .h files
    
    * closes task #185
    
    unnecessary
    
    * closes task #184
    
    unnecessary
    
    * closes task #186
    
    After working on this I believe a stats class is redundant. All the information we want to modify already exists in the RunnerWheel.cpp. Calling another class to handle decision making that as far as i can tell is only ever referenced in one place in unnecessary
    
    * added default case

--- Source/BroncoDrome/Runner/RunnerWheel.cpp
+++ Source/BroncoDrome/Runner/RunnerWheel.cpp
@@ -11,16 +11,64 @@ URunnerWheel::URunnerWheel()
 	bAffectedByEngine = true;
 	bTractionControlEnabled = true;
 
-	// Other attributes
-	WheelRadius = 15.f;
-	WheelWidth = 30.f;
-	LateralFrictionForceMultiplier = 8.f;
-	SideSlipModifier = 1.f;
-	SlipThreshold = 12.f;
-	SkidThreshold = 12.f;
-	SuspensionDampingRatio = 2.f;
-	MaxSteerAngle = 22.5f;
-	SpringRate = 750.f;
-	SuspensionMaxRaise = 15.f;
-	SuspensionMaxDrop = 15.f;
+	enum RunnerStats{Speed, Traction, Balanced};
+	RunnerStats runnerSelected = Balanced;
+
+	Switch(runnerSelected)
+	{
+		case(Speed):
+			WheelRadius = 10.f;
+			WheelWidth = 30.f;
+			LateralFrictionForceMultiplier = 4.f;
+			SideSlipModifier = 1.f;
+			SlipThreshold = 8.f;
+			SkidThreshold = 8.f;
+			SuspensionDampingRatio = 1.f;
+			MaxSteerAngle = 27.5f;
+			SpringRate = 550.f;
+			SuspensionMaxRaise = 10.f;
+			SuspensionMaxDrop = 10.f;
+			break;
+		case(Traction):
+			WheelRadius = 20.f;
+			WheelWidth = 35.f;
+			LateralFrictionForceMultiplier = 10.f;
+			SideSlipModifier = 1.f;
+			SlipThreshold = 15.f;
+			SkidThreshold = 15.f;
+			SuspensionDampingRatio = 5.f;
+			MaxSteerAngle = 17.5f;
+			SpringRate = 1000.f;
+			SuspensionMaxRaise = 20.f;
+			SuspensionMaxDrop = 20.f;
+			break;
+		case(Balanced):
+			// Other attributes
+			WheelRadius = 15.f;
+			WheelWidth = 30.f;
+			LateralFrictionForceMultiplier = 8.f;
+			SideSlipModifier = 1.f;
+			SlipThreshold = 12.f;
+			SkidThreshold = 12.f;
+			SuspensionDampingRatio = 2.f;
+			MaxSteerAngle = 22.5f;
+			SpringRate = 750.f;
+			SuspensionMaxRaise = 15.f;
+			SuspensionMaxDrop = 15.f;
+			break;
+		Default:
+			WheelRadius = 15.f;
+			WheelWidth = 30.f;
+			LateralFrictionForceMultiplier = 8.f;
+			SideSlipModifier = 1.f;
+			SlipThreshold = 12.f;
+			SkidThreshold = 12.f;
+			SuspensionDampingRatio = 2.f;
+			MaxSteerAngle = 22.5f;
+			SpringRate = 750.f;
+			SuspensionMaxRaise = 15.f;
+			SuspensionMaxDrop = 15.f;
+			break;
+	}
+
 }
